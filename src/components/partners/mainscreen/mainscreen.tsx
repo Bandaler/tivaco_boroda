3053cd9cb4ebae797d1b1b5f32e66533c52324c5
kali <kali@kali.kali>
2025-10-22 21:04:12 +0300

    migratev6

--- src/components/partners/mainscreen/mainscreen.tsx
+++ src/components/partners/mainscreen/mainscreen.tsx
@@ -2,7 +2,7 @@ import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
 import PartnersSlider from "../slider/PartnersSlider";
 
 export default async function PartnersHomepage() {
-  const API_URL = process.env.NEXT_PUBLIC_NEXT_PUBLIC_API_SECRET_URL_PAGES;
+  const API_URL = process.env.NEXT_PUBLIC_API_SECRET_URL_PAGES;
   const res = await fetch(`${API_URL}/218?_embed`, { cache: 'force-cache' });
 
   const pageData = await res.json();
