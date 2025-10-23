ed49512d57f00e4cccb95af3577743a3d6911da2
kali <kali@kali.kali>
2025-10-23 23:33:21 +0300

    fix fixes thu

--- src/app/news-articles/page.tsx
+++ src/app/news-articles/page.tsx
@@ -16,6 +16,7 @@ export default async function News() {
       <main className="scroll-container">
         <section className="section light portfolio-section__parent">
           <Mainscreen />
+          
         </section>
       </main>
     </>
